a657335eb6173447312891d775bd182acbb62f48
MR3730415 <m3n7m3.97.415@gmail.com>
2021-10-18 13:46:37 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/edit'
-  get 'books/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+  resources :books
 end
